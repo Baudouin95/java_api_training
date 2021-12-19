7db7dad6a0cd07388a583f873c7e00efc2c4d9cf
Lavoile <arnaudkerry@gmail.com>
2021-12-19 10:42:38 +0100

    Launcher class creation

---
+++ src/main/java/fr/lernejo/navy_battle/Launcher.java
@@ -0,0 +1,14 @@
+package fr.lernejo.navy_battle;
+
+import com.sun.net.httpserver.HttpServer;
+
+import java.io.IOException;
+import java.util.Collection;
+import java.util.List;
+import java.util.concurrent.*;
+
+public class Launcher {
+    public static void main(String[] args) {
+
+    }
+}
