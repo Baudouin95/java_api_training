d9987ee9ad878ccbe46614aac3e820e5fe39fd20
Baudouin95 <tekoubo@gmail.com>
2021-12-20 01:31:58 +0100

    Add HttpClient

--- src/main/java/fr/lernejo/navy_battle/Launcher.java
+++ src/main/java/fr/lernejo/navy_battle/Launcher.java
@@ -9,7 +9,10 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.InetSocketAddress;
+import java.net.URI;
 import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 import java.nio.charset.StandardCharsets;
 import java.util.Collection;
 import java.util.List;
@@ -19,12 +22,27 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Launcher {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException, InterruptedException {
             HttpServer server = HttpServer.create(new InetSocketAddress(Integer.parseInt(args[0])), 0);
             server.setExecutor(Executors.newFixedThreadPool(1));
             server.createContext("/ping", new MyHttpHandler());
             server.createContext("/api/game/start", new POSTHandler());
             server.start();
+
+        if(args.length > 1 && args[1].matches("(https?|ftp|ssh|mailto):\\/\\/[a-z0-9\\/:%_+.,#?!@&=-]+"))
+        {
+            UUID id = UUID.randomUUID();
+            String body = "\"id\": \"0c575465-21f6-43c9-8a2d-bc64c3ae6241\", \"url\": \"http://localhost:8795\", \"message\": \"I will crush you!\"";
+            HttpClient client = HttpClient.newHttpClient();
+            HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(args[1]+"/api/game/start"))
+                .setHeader("Accept","application/json")
+                .setHeader("Content-Type", "application/json")
+                .POST(HttpRequest.BodyPublishers.ofString(body))
+                .build();
+            HttpResponse response = client.send(request, HttpResponse.BodyHandlers.ofString());
+            System.out.println(response.body());
+        }
     }
 
     static class MyHttpHandler implements HttpHandler {
@@ -91,9 +109,9 @@ public class Launcher {
         }
 
         private boolean isFormatCorrect(String response) {
-            Pattern patternID = Pattern.compile("\"id\": \"[a-f0-9]{8}-[a-f0-9]{4}-4[a-f0-9]{3}-[89aAbB][a-f0-9]{3}-[a-f0-9]{12}\",");
-            Pattern patternURL = Pattern.compile("\"url\": \"(https?|ftp|ssh|mailto):\\/\\/[a-z0-9\\/:%_+.,#?!@&=-]+\",");
-            Pattern patternMessage = Pattern.compile("\"message\": \"(.*)\"");
+            Pattern patternID = Pattern.compile("\"id\":\s\"[a-f0-9]{8}-[a-f0-9]{4}-4[a-f0-9]{3}-[89aAbB][a-f0-9]{3}-[a-f0-9]{12}\",");
+            Pattern patternURL = Pattern.compile("\"url\":\s\"(https?|ftp|ssh|mailto):\\/\\/[a-z0-9\\/:%_+.,#?!@&=-]+\",");
+            Pattern patternMessage = Pattern.compile("\"message\":\s\"(.*)\"");
             Matcher matchID = patternID.matcher(response);
             Matcher matchURL = patternURL.matcher(response);
             Matcher matchMessage = patternMessage.matcher(response);
